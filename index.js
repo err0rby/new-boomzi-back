f0b2ec8b512df321bccda02ff471d9b6925fac02
shishanee <mansur72095@gmail.com>
2023-09-09 00:12:57 +0300

    Первоначальная настройка

---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+app.use("/images", express.static(__dirname + "/images"));
+app.use(cors());
+
+mongoose
+  .connect(
+    process.env.MONGO,
+
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Успешно соединились с сервером MongoDB"))
+  .catch(() => console.log("Ошибка при соединении с сервером MongoDB"));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Сервер запущен успешно на порте ${process.env.PORT}`);
+});
